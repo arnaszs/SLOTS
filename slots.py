3bbaa03aa7bcd325c6f08592331443a0ffde0e01
arnaszs <bolisas8@gmail.com>
2024-01-03 16:47:30 +0200

    item generation functionality

--- slots.py
+++ slots.py
@@ -1,6 +1,39 @@
+import random
+
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8,
+}
+# Generating items that are gonna be in our slot machine
+def get_slot_machine_outcome(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = [[], [], []]
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:] # Copy of all symbols
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+        
+        columns.append(column)
+
+    return columns
+
 
 # Deposit functionality
 def deposit ():
@@ -51,8 +84,17 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
-    print(balance, lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet >= balance:
+            print(f"You don't have enough money, you current balance is ${balance}")
+        else:
+            break
+
+
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to : ${total_bet}")
 
 
 main()
